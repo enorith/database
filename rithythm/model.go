d52b91569d4c977ee23a195d77cdca779694bd7f
caojiayuan <cjy632258@hotmail.com>
2019-03-04 16:33:59 +0800

    model

--- rithythm/model.go
+++ rithythm/model.go
@@ -3,6 +3,8 @@ package rithythm
 import (
 	"encoding/json"
 	"github.com/CaoJiayuan/goutilities/define"
+	"errors"
+	"fmt"
 )
 
 type DataModel interface {
@@ -12,6 +14,8 @@ type DataModel interface {
 	marshal(data define.Map)
 	Clone() DataModel
     GetValue(field string) interface{}
+	GetString(field string) (string, error)
+	GetInt(field string) (int, error)
 }
 
 type Model struct {
@@ -40,6 +44,22 @@ func (m *Model) GetKeyName() string {
 
 func (m *Model) GetValue(field string) interface{} {
 	return m.originals[field]
+}
+
+func (m *Model) GetString(field string) (string, error) {
+	if s, ok := m.GetValue(field).(string); ok {
+		return s, nil
+	}
+
+	return "", errors.New(fmt.Sprintf("try to get string value from field: %s", field))
+}
+
+func (m *Model) GetInt(field string) (int, error) {
+	if i, ok := m.GetValue(field).(int); ok {
+		return i, nil
+	}
+
+	return 0, errors.New(fmt.Sprintf("try to get int value from field: %s", field))
 }
 
 func (m *Model) marshal(data define.Map) {
